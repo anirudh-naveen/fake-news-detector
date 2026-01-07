6fbcd3ecf742c2eed3bdd456ae5a54e6b5c7fe0b
anirudhnaveen@outlook.com <anirudhnaveen@outlook.com>
2026-01-06 23:24:26 -0500

    Started news scraper bot code

---
+++ newscraper.py
@@ -0,0 +1,44 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_KEY = os.getenv('NEWS_API_KEY')
+BASE_URL = 'https://newsapi.org/v2/'
+
+if not API_KEY:
+    raise ValueError("NEWS_API_KEY not found")
+
+def scrape_real_news(query, pages=10):
+    articles = []
+
+    for page in range(1, pages + 1):
+        url = f"{BASE_URL}everything"
+        params = {
+            'q': query,
+            'sources': 'bbc-news,reuters,associated-press',
+            'apiKey': API_KEY,
+            'page': page,
+            'pageSize': 100,
+            'language': 'en'
+        }
+
+        response = requests.get(url, params=params)
+        data = response.json()
+
+        if 'articles' in data:
+            for article in data['articles']:
+                if article['title'] and article['content']:
+                    articles.append({
+                        'title': article['title'],
+                        'data': article['content'] or article['description'],
+                        'label': 'REAL'
+                    })
+        
+        time.sleep(1)       # rate limiting checkpoint
+        print(f"Scraped {page}. At {len(articles)} news articles.")
+
+    return pd.DataFrame(articles)
+
+real_news = scrape_real_news('politics', pages=20)
+real_news.to_csv('scraped_real_news.csv', index=False)
